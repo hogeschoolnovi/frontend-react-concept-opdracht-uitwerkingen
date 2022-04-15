d2f5e24a1bfdb718f6595d52ce1b3bcfbcc55e27
novaeeken <nova_eeken@hotmail.com>
2022-04-15 17:39:46 +0200

    Implement routing

--- src/App.js
+++ src/App.js
@@ -1,11 +1,27 @@
 import React from 'react';
+import { Switch, Route } from 'react-router-dom';
+import SubredditPage from './pages/subreddit/SubredditPage';
+import HomePage from './pages/home/HomePage';
 import './App.css';
 
 function App() {
   return (
-    <div>
-        Let's start our app!
-    </div>
+    <>
+      <Switch>
+        <Route exact path="/">
+          <HomePage />
+        </Route>
+        <Route path="/subreddit/:id">
+          <SubredditPage />
+        </Route>
+      </Switch>
+
+      <footer className="outer-content-container">
+        <div className="inner-content-container">
+          In opdracht van NOVI Hogeschool © 2022
+        </div>
+      </footer>
+    </>
   );
 }
 
