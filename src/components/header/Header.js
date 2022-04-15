182099a55aa3ab324c0ec2ce212b742c650abc92
novaeeken <nova_eeken@hotmail.com>
2022-04-15 17:41:19 +0200

    Add Header component and general styling

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,11 +1,23 @@
 import React from 'react';
 import './Header.css';
+import { Link } from 'react-router-dom';
 
-function Header(props) {
+function Header({ children }) {
   return (
-    <div>
-
-    </div>
+    <header className="outer-content-container">
+      <div className="inner-content-container">
+        <nav>
+          <ul>
+            <li><Link to="/">Hottest posts</Link></li>
+            <li><a href="https://www.reddit.com/">Reddit</a></li>
+            <li><Link to="/subreddit/memes">Memes</Link></li>
+          </ul>
+        </nav>
+        <div className="hero-content">
+          {children}
+        </div>
+      </div>
+    </header>
   );
 }
 
