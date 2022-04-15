077c176bf65d666af2661b267c61dc592680d501
novaeeken <nova_eeken@hotmail.com>
2022-04-15 17:45:17 +0200

    Fetch data and display it on subreddit

--- src/pages/subreddit/SubredditPage.js
+++ src/pages/subreddit/SubredditPage.js
@@ -1,11 +1,27 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { Link, useParams } from 'react-router-dom';
+import axios from 'axios';
 import './SubredditPage.css';
 import Header from '../../components/header/Header';
 import { ReactComponent as BackIcon } from '../../assets/back.svg';
 
 function SubredditPage() {
+  const [details, setDetails] = useState({});
   const { id } = useParams();
+
+  useEffect(() => {
+    async function fetchData() {
+      try {
+        const response = await axios.get(`https://www.reddit.com/r/${id}/about.json`);
+        console.log(response);
+        setDetails(response.data.data);
+      } catch (e) {
+        console.error(e);
+      }
+    }
+
+    fetchData();
+  }, []);
 
   return (
     <>
@@ -16,19 +32,21 @@ function SubredditPage() {
       <main>
         <section className="outer-content-container subreddit-specifications">
           <div className="inner-content-container">
-
+            {Object.keys(details).length > 0 && (
               <div className="subreddit-specification-details">
                 <h3>Title</h3>
-
+                <p>{details.title}</p>
                 <h3>Description</h3>
-
+                <p>{details.public_description}</p>
                 <h3>Number of subscribers</h3>
+                <p>{details.subscribers}</p>
 
                 <span className="back-link-wrapper">
                   <BackIcon className="back-icon"/>
                   <Link to="/">Take me back</Link>
                 </span>
               </div>
+            )}
           </div>
         </section>
       </main>
