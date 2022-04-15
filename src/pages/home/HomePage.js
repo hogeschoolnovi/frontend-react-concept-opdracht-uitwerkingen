0465d2e7eb594856eda8c1e0581ad5f318eb72d7
novaeeken <nova_eeken@hotmail.com>
2022-04-15 17:44:08 +0200

    Create page structure and page-specific styling

--- src/pages/home/HomePage.js
+++ src/pages/home/HomePage.js
@@ -1,11 +1,27 @@
 import React from 'react';
 import './HomePage.css';
+import Header from '../../components/header/Header';
+import logo from '../../assets/logo.png';
 
-function HomePage(props) {
+function HomePage() {
   return (
-    <div>
+    <>
+      <Header>
+        <img src={logo} alt="Reddit header logo"/>
+        <h1>Reddit</h1>
+      </Header>
+      <main>
+        <section className="outer-content-container hottest-posts">
+          <div className="inner-content-container">
+            <h2>Hottest posts</h2>
+            <h4>on Reddit right now</h4>
+            <div className="subreddit-article-container">
 
-    </div>
+            </div>
+          </div>
+        </section>
+      </main>
+    </>
   );
 }
 
--- src/pages/subreddit/SubredditPage.js
+++ src/pages/subreddit/SubredditPage.js
@@ -1,11 +1,38 @@
 import React from 'react';
+import { Link, useParams } from 'react-router-dom';
 import './SubredditPage.css';
+import Header from '../../components/header/Header';
+import { ReactComponent as BackIcon } from '../../assets/back.svg';
 
-function SubredditPage(props) {
+function SubredditPage() {
+  const { id } = useParams();
+
   return (
-    <div>
+    <>
+      <Header>
+        <h1>r/{id}</h1>
+        <h4>Subreddit specifications</h4>
+      </Header>
+      <main>
+        <section className="outer-content-container subreddit-specifications">
+          <div className="inner-content-container">
 
-    </div>
+              <div className="subreddit-specification-details">
+                <h3>Title</h3>
+
+                <h3>Description</h3>
+
+                <h3>Number of subscribers</h3>
+
+                <span className="back-link-wrapper">
+                  <BackIcon className="back-icon"/>
+                  <Link to="/">Take me back</Link>
+                </span>
+              </div>
+          </div>
+        </section>
+      </main>
+    </>
   );
 }
 
