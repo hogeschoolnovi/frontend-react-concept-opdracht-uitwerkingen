3ea5e768159bb0dd9226a879f8e4a63f4378f816
novaeeken <nova_eeken@hotmail.com>
2022-04-15 17:44:48 +0200

    Fetch data and display it on home

--- src/pages/home/HomePage.js
+++ src/pages/home/HomePage.js
@@ -1,9 +1,27 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import './HomePage.css';
+import axios from 'axios';
+import { Link } from 'react-router-dom';
 import Header from '../../components/header/Header';
 import logo from '../../assets/logo.png';
 
 function HomePage() {
+  const [reddits, setReddits] = useState([]);
+
+  useEffect(() => {
+    async function fetchData() {
+      try {
+        const response = await axios.get('https://www.reddit.com/hot.json?limit=15');
+        console.log(response);
+        setReddits(response.data.data.children);
+      } catch (e) {
+        console.error(e);
+      }
+    }
+
+    fetchData();
+  }, [])
+
   return (
     <>
       <Header>
@@ -16,7 +34,22 @@ function HomePage() {
             <h2>Hottest posts</h2>
             <h4>on Reddit right now</h4>
             <div className="subreddit-article-container">
+              {reddits.map((reddit) => {
+                return (
+                  <article className="subreddit-article" key={reddit.data.id}>
 
+                    {/*We gebruiken hier een <a> ipv <Link> omdat we naar een webpagina buiten onze applicatie linken */}
+                    <a href={reddit.data.url} className="subreddit-article-title-link">
+                      <h3>{reddit.data.title}</h3>
+                    </a>
+
+                    <span>
+                      <p><Link to={`/subreddit/${reddit.data.subreddit}`}>{reddit.data.subreddit_name_prefixed}</Link></p>
+                      <p>Comments {reddit.data.num_comments} — Ups {reddit.data.ups}</p>
+                    </span>
+                  </article>
+                )
+              })}
             </div>
           </div>
         </section>
